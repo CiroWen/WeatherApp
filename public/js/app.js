afd877829132960cc4ab94621126e4f0ecf31b76
cero9406@gmail.com <cero9406@gmail.com>
2021-01-06 18:42:29 -0800

    Heroku setup

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ console.log(`Client side js is loaded`);
 //     })
 // })
 
-fetch(`http://localhost:3000/weather?address=boston`).then((res)=>{
+fetch(`/weather?address=boston`).then((res)=>{
     res.json().then((data)=>{
         if(data.error){
             console.log(data.error);
